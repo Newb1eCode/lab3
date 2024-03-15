a4dc8d8caeabfe481b483a06766501ff3fa0ae83
PresidentHuTao <pmhoang29084@gmail.com>
2024-03-15 10:23:24 +0700

    a

--- src/test/java/bai4Test.java
+++ src/test/java/bai4Test.java
@@ -1,11 +1,18 @@
-import org.junit.Test;
+import  org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class bai4Test {
 
     @Test
-    public void getElementAtIndex() {
+    public void testGetElementAtIndex() throws Exception{
+        bai4 i = new bai4();
+        try {
+            String result = i.getElementAtIndex(129);
+            fail("Expected an ArithmeticException to be thrown");
+        } catch (ArithmeticException e) {
+            assertEquals(e.getMessage(), "out of bounds");
+        }
     }
 
 }
